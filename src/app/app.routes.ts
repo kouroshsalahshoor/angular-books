df9c32e826092557154ee4ed9b9d6d43cbffebfe
kourosh <kourosh23@hotmail.com>
2025-11-25 11:15:03 +0100

    components added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Register } from './components/register/register';
+import { BooksList } from './components/books-list/books-list';
+import { BookForm } from './components/book-form/book-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: 'books', component: BooksList },
+  { path: 'book/add', component: BookForm },
+  { path: 'book/edit/:id', component: BookForm },
+  { path: '', redirectTo: '/books', pathMatch: 'full' },
+  { path: '**', redirectTo: '/books' },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { BooksList } from './components/books-list/books-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, BooksList],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('angular-books');
-  protected readonly title2 = 'angular-books title 2';
 }
