b5c5fbaa794355788892ad0a2b06954d0f0f98bf
kourosh <kourosh23@hotmail.com>
2025-11-25 22:24:36 +0100

    books list

--- src/app/components/books-list/books-list.ts
+++ src/app/components/books-list/books-list.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Book } from '../../models/book';
+import { BooksService } from '../../services/books-service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-books-list',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './books-list.html',
   styleUrl: './books-list.css',
 })
-export class BooksList {
-  items: Book[] = [
-    { id: 1, title: 'Sample Book', author: 'Author Name', publishedDate: new Date() },
-    { id: 2, title: 'Another Book', author: 'Another Author', publishedDate: new Date() },
-    { id: 3, title: 'Third Book', author: 'Third Author', publishedDate: new Date() },
-  ];
+export class BooksList implements OnInit {
+  items: Book[] = [];
+  // items: Book[] = [
+  //   { id: 1, title: 'Sample Book', author: 'Author Name', publishedDate: new Date() },
+  //   { id: 2, title: 'Another Book', author: 'Another Author', publishedDate: new Date() },
+  //   { id: 3, title: 'Third Book', author: 'Third Author', publishedDate: new Date() },
+  // ];
+
+  constructor(private service: BooksService) {}
+
+  ngOnInit(): void {
+    this.load();
+  }
+
+  load(): void {
+    this.service.getAll().subscribe((data) => (this.items = data));
+  }
 }
