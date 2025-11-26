329eed518ad27565b312fcc66fa89fab5384537c
kourosh <kourosh23@hotmail.com>
2025-11-26 13:00:14 +0100

    book crud

--- src/app/components/books-list/books-list.ts
+++ src/app/components/books-list/books-list.ts
@@ -4,16 +4,16 @@ import { BooksService } from '../../services/books-service';
 import { AsyncPipe, DatePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { mapToView } from '../../utils/book-mapper';
-import { map, Observable } from 'rxjs';
+import { map, Observable, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-books-list',
   imports: [DatePipe, RouterLink, AsyncPipe],
   templateUrl: './books-list.html',
-  styleUrl: './books-list.css',
+  styleUrls: ['./books-list.css'],
 })
 export class BooksList {
-  items$: Observable<Book[]>;
+  items$!: Observable<Book[]>;
   // items: Book[] = [];
   // items: Book[] = [
   //   { id: 1, title: 'Sample Book', author: 'Author Name', publishedDate: new Date() },
@@ -22,9 +22,21 @@ export class BooksList {
   // ];
 
   constructor(private service: BooksService) {
+    this.load();
+  }
+
+  load(): void {
     this.items$ = this.service
       .getAll()
-      .pipe(map((data) => data.map((book: any) => mapToView(book))));
-    // this.items$ = this.service.getAll().pipe(map((data) => [].map((book: any) => mapToView(book))));
+      .pipe(map((data) => data.map((item: any) => mapToView(item))));
+  }
+
+  delete(id: number) {
+    if (confirm('Are you sure you want to delete this item?')) {
+      this.items$ = this.service.delete(id).pipe(
+        switchMap(() => this.service.getAll()),
+        map((data) => data.map((item: any) => mapToView(item)))
+      );
+    }
   }
 }
--- src/app/utils/book-mapper.ts
+++ src/app/utils/book-mapper.ts
@@ -1,33 +1,41 @@
 // book-mapper.ts
 
-// Mapper: API → Form
-export function mapToView(data: any): any {
+export function mapToView(item: any): any {
   return {
-    id: data.id,
-    title: data.title,
-    author: data.author,
-    publishedDate: formatDateForInput(data.publishedDate),
+    ...item,
+    publishedDate: formatDateForInput(item.publishedDate),
   };
+  // return {
+  //   id: item.id,
+  //   title: item.title,
+  //   author: item.author,
+  //   publishedDate: formatDateForInput(item.publishedDate),
+  // };
 }
 
-// Mapper: Form → API
-export function mapFrom(formValue: any): any {
+export function mapFrom(item: any): any {
   return {
-    id: formValue.id,
-    title: formValue.title,
-    author: formValue.author,
-    publishedDate: formatDateForApi(formValue.publishedDate),
+    ...item,
+    publishedDate: formatDateForApi(item.publishedDate),
   };
+
+  // return {
+  //   id: item.id,
+  //   title: item.title,
+  //   author: item.author,
+  //   publishedDate: formatDateForApi(item.publishedDate),
+  // };
 }
 
 // Utility: format date for <input type="date">
 export function formatDateForInput(dateValue: string | Date): string {
-  const date = new Date(dateValue);
+  const date = new Date(dateValue + 'Z');
   return date.toISOString().split('T')[0]; // "yyyy-MM-dd"
 }
 
 // Utility: format date for API (ISO string)
 export function formatDateForApi(dateValue: string): string {
-  const date = new Date(dateValue);
+  const date = new Date(dateValue + 'T00:00:00Z');
+  console.log(date);
   return date.toISOString(); // "yyyy-MM-ddTHH:mm:ss.sssZ"
 }
